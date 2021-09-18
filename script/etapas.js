f61d555878062462811bfbd0aaac568776e272a8
Lucas Cabaleiro <79326445+olucas07@users.noreply.github.com>
2021-09-18 19:15:04 -0300

    Add files via upload

---
+++ script/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '12212',
+                nome: 'Agustinho Carrara',
+                partido: 'GTA',
+                fotos: [
+                    { url: '6.jpg', legenda: 'Vereador', small: false }
+                ]
+            },
+            {
+                numero: '20210',
+                nome: 'Ronaldo',
+                partido: 'SCCP',
+                fotos: [
+                    { url: '5.jpg', legenda: 'Vereador' }
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '12',
+                nome: 'Michael',
+                partido: 'GTA',
+                vice: 'Franklin',
+                fotos: [
+                    { url: '2.png', legenda: 'Prefeito' },
+                    { url: '3.png', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+            {
+                numero: '20',
+                nome: 'Neto',
+                partido: 'SCCP',
+                vice: 'Edilson',
+                fotos: [
+                    { url: '1.png', legenda: 'Prefeito' },
+                    { url: '4.jpg', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+        ]
+    }
+];
